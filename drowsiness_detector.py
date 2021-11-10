94c32dfc4ecea279733c94dc7aac1bd0b4f044af
Judit <yuhalper@g.jct.ac.il>
2021-11-10 10:52:11 +0200

    import packages

---
+++ drowsiness_detector.py
@@ -0,0 +1,16 @@
+
+# Drowsiness detector algorithm
+
+
+# import packages
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import dlib
+import cv2
