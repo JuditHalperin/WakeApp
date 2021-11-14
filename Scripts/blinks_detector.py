e1ed8ffcd992efe7e69758a577b9faef673c6e17
Judit <yuhalper@g.jct.ac.il>
2021-11-14 23:11:37 +0200

    blink detector

--- Scripts/blinks_detector.py
+++ Scripts/blinks_detector.py
@@ -22,16 +22,16 @@ def eye_aspect_ratio(eye):
 
 
 def visualize_eyes(frame, leftEye, rightEye):
-    """compute the convex hull for the left and right eye, then visualize each of the eyes"""
-    leftEyeHull = cv2.convexHull(leftEye)
-    rightEyeHull = cv2.convexHull(rightEye)
-    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+    """compute the convex hull for the left and right eye, then visualize each of the eyes on the frame"""
+    cv2.drawContours(frame, [cv2.convexHull(leftEye)], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [cv2.convexHull(rightEye)], -1, (0, 255, 0), 1)
+    return frame
 
 
 def compute_blinks_score(frame, shape):
     """compute the blinks score"""
     leftEye, rightEye = shape[lStart:lEnd], shape[rStart:rEnd]  # extract the left and right eye coordinates
     leftEAR, rightEAR = eye_aspect_ratio(leftEye), eye_aspect_ratio(rightEye)  # compute the eye aspect ratios
-    visualize_eyes(frame, leftEye, rightEye)
-    return (leftEAR + rightEAR) / 2.0  # average the eye aspect ratios and return the blinks score
+    score = (leftEAR + rightEAR) / 2.0  # average the eye aspect ratios
+    frame = visualize_eyes(frame, leftEye, rightEye)  # visualize the eyes on the frame
+    return score, frame  # return the blinks score and the frame
