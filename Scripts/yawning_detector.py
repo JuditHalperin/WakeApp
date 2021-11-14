1e824d25210da1e773c9d5088c113619c621c2fd
Judit <yuhalper@g.jct.ac.il>
2021-11-14 23:09:25 +0200

    yawning structure

--- Scripts/yawning_detector.py
+++ Scripts/yawning_detector.py
@@ -1,5 +1,16 @@
 
+# Yawning detector algorithm
 
-def compute_yawning_score():
+
+def visualize_lips(frame):
+    """visualize the lips on the frame"""
+    # MARK THE MOUTH IN THE FRAME #
+    return frame
+
+
+def compute_yawning_score(frame, shape):
     """compute the yawning score"""
-    # return score
+    # COMPUTE SCORE HERE #
+    score = 12345  # compute the yawning score
+    frame = visualize_lips(frame)  # visualize the lips on the frame
+    return score, frame  # return the yawning score and the frame
