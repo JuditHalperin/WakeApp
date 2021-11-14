8d17bc8db0ef033c9767f6316ce29cc9e7d8573f
נעמה שנברגר <shanberg@g.jct.ac.il>
2021-11-14 09:23:29 +0200

    mouth open? code

--- drowsiness_detector.py
+++ drowsiness_detector.py
@@ -1,150 +1,68 @@
+#https://towardsdatascience.com/how-to-detect-mouth-open-for-face-login-84ca834dff3b
+from PIL import Image, ImageDraw
+import face_recognition
+import math
+def get_lip_height(lip):
+    sum=0
+    for i in [2,3,4]:
+        # distance between two near points up and down
+        distance = math.sqrt( (lip[i][0] - lip[12-i][0])**2 +
+                              (lip[i][1] - lip[12-i][1])**2   )
+        sum += distance
+    return sum / 3
+def get_mouth_height(top_lip, bottom_lip):
+    sum=0
+    for i in [8,9,10]:
+        # distance between two near points up and down
+        distance = math.sqrt( (top_lip[i][0] - bottom_lip[18-i][0])**2 +
+                              (top_lip[i][1] - bottom_lip[18-i][1])**2   )
+        sum += distance
+    return sum / 3
+def check_mouth_open(top_lip, bottom_lip):
+    top_lip_height = get_lip_height(top_lip)
+    bottom_lip_height = get_lip_height(bottom_lip)
+    mouth_height = get_mouth_height(top_lip, bottom_lip)
 
-# Drowsiness detector algorithm
-# try:)
-#conflict
-# try
-#hey
-# import packages
-from certifi.__main__ import args
-from scipy.spatial import distance as dist
-from imutils.video import VideoStream
-from imutils import face_utils
-from threading import Thread
-import numpy as np
-import playsound
-import argparse
-import imutils
-import time
-import dlib
-import cv2
+    # if mouth is open more than lip height * ratio, return true.
+    ratio = 0.5
+    if mouth_height > min(top_lip_height, bottom_lip_height) * ratio:
+        return True
+    else:
+        return False
+image_file = 'obama.jpg'
 
+# Load the jpg file into a numpy array
+image = face_recognition.load_image_file(image_file)
 
-def sound_alarm(path):
-    # play an alarm sound
-    playsound.playsound(path)
+# Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
 
+print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
 
-def eye_aspect_ratio(eye):
-    # compute the euclidean distances between the two sets of
-    # vertical eye landmarks (x, y)-coordinates
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
-    # compute the euclidean distance between the horizontal
-    # eye landmark (x, y)-coordinates
-    C = dist.euclidean(eye[0], eye[3])
-    # compute the eye aspect ratio
-    ear = (A + B) / (2.0 * C)
-    # return the eye aspect ratio
-    return ear
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
 
+for face_landmarks in face_landmarks_list:
+    # Print the location of each facial feature in this image
+    facial_features = [
+        'chin',
+        'left_eyebrow',
+        'right_eyebrow',
+        'nose_bridge',
+        'nose_tip',
+        'left_eye',
+        'right_eye',
+        'top_lip',
+        'bottom_lip'
+    ]
 
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold for to set off the
-# alarm
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 48
-# initialize the frame counter as well as a boolean used to
-# indicate if the alarm is going off
-COUNTER = 0
-ALARM_ON = False
+    for facial_feature in facial_features:
+        print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
 
+    # Let's trace out each facial feature in the image with a line!
+    for facial_feature in facial_features:
+        d.line(face_landmarks[facial_feature], width=5)
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
-
-
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-
-
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-vs = VideoStream(src=args["webcam"]).start()
-time.sleep(1.0)
-
-
-# loop over frames from the video stream
-while True:
-    # grab the frame from the threaded video file stream, resize
-    # it, and convert it to grayscale
-    # channels)
-    frame = vs.read()
-    frame = imutils.resize(frame, width=450)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # detect faces in the grayscale frame
-    rects = detector(gray, 0)
-
-    # loop over the face detections
-    for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-        leftEAR = eye_aspect_ratio(leftEye)
-        rightEAR = eye_aspect_ratio(rightEye)
-        # average the eye aspect ratio together for both eyes
-        ear = (leftEAR + rightEAR) / 2.0
-
-        # compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-
-        # check to see if the eye aspect ratio is below the blink
-        # threshold, and if so, increment the blink frame counter
-        if ear < EYE_AR_THRESH:
-            COUNTER += 1
-            # if the eyes were closed for a sufficient number of
-            # then sound the alarm
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                # if the alarm is not on, turn it on
-                if not ALARM_ON:
-                    ALARM_ON = True
-                    # check to see if an alarm file was supplied,
-                    # and if so, start a thread to have the alarm
-                    # sound played in the background
-                    if args["alarm"] != "":
-                        t = Thread(target=sound_alarm, args=(args["alarm"],))
-                        t.deamon = True
-                        t.start()
-                # draw an alarm on the frame
-                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-        # otherwise, the eye aspect ratio is not below the blink
-        # threshold, so reset the counter and alarm
-        else:
-            COUNTER = 0
-            ALARM_ON = False
-
-            # draw the computed eye aspect ratio on the frame to help
-            # with debugging and setting the correct eye aspect ratio
-            # thresholds and frame counters
-            cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-    # show the frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+# Display drawed image
+pil_image.show()
+#　pil_image.save('test.png')
