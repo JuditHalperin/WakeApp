b851ea59df863d4c1b68312326379fc282ac4bd9
Judit <yuhalper@g.jct.ac.il>
2021-11-15 11:48:23 +0200

    run

--- Scripts/drowsiness_classification.py
+++ Scripts/drowsiness_classification.py
@@ -22,12 +22,9 @@ import time_detector
 WORKING_DIRECTORY = os.getcwd().replace("\\", "/").replace("Scripts", "")  # path to working directory
 SHAPE_PREDICTOR = WORKING_DIRECTORY + "Data/shape_predictor_68_face_landmarks.dat"  # path to facial landmark predictor
 ALARM = WORKING_DIRECTORY + "Data/bigwarning.wav"  # path alarm .WAV file
-WEBCAM = 0  # index of webcam on system
 
 DROWSINESS_SCORE_THRESHOLD = 12345  # CHOOSE THRESHOLD  # drowsiness score threshold
 FRAMES_THRESHOLD = 48  # number of frames threshold
-COUNTER = 0  # consecutive frames where the drowsiness score is above threshold
-ALARM_ON = False  # boolean variable indicating whether the alarm is on or off
 
 
 def compute_drowsiness_score(frame, shape):
@@ -49,53 +46,52 @@ def send_mail(name, address):
     # SEND MAIL #
 
 
-# initialize dlib's face detector (HOG-based) and then create the facial landmark predictor
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
+def run(webcam=0):
+    """main function looping video stream. webcom indicates the index of webcam on system"""
 
-# start the video stream thread
-vs = VideoStream(src=WEBCAM).start()
-time.sleep(1.0)  # pause for a second to allow the camera sensor to warm up
+    counter = 0  # consecutive frames where the drowsiness score is above threshold
+    alarm_on = False  # boolean variable indicating whether the alarm is on or off
 
+    detector = dlib.get_frontal_face_detector()  # initialize dlib's face detector (HOG-based)
+    predictor = dlib.shape_predictor(SHAPE_PREDICTOR)  # create facial landmark predictor
+    vs = VideoStream(src=webcam).start()  # start the video stream thread
+    time.sleep(1.0)  # pause for a second to allow the camera sensor to warm up
 
-# loop over frames from the video stream
-while True:
+    while True:  # loop over frames from the video stream
 
-    frame = vs.read()  # grab the frame from the threaded video file stream
-    frame = imutils.resize(frame, width=450)  # resize the frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale channels
+        frame = vs.read()  # grab the frame from the threaded video file stream
+        frame = imutils.resize(frame, width=450)  # resize the frame
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale channels
 
-    face = detector(gray, 0)  # detect faces in the grayscale frame - assuming there is only one face
-    if not face: continue  # DO SOMETHING #
-    shape = predictor(gray, face[0])  # determine the facial landmarks for the face region
-    shape = face_utils.shape_to_np(shape)  # convert the facial landmark (x, y)-coordinates to a NumPy array
+        face = detector(gray, 0)  # detect faces in the grayscale frame - assuming there is only one face
+        if not face: continue  # DO SOMETHING #
+        shape = predictor(gray, face[0])  # determine the facial landmarks for the face region
+        shape = face_utils.shape_to_np(shape)  # convert the facial landmark (x, y)-coordinates to a NumPy array
 
-    # compute the drowsiness score based on blink, yawning, duration and time
-    drowsiness_score, frame = compute_drowsiness_score(frame, shape)
-    print(drowsiness_score)
+        drowsiness_score, frame = compute_drowsiness_score(frame, shape)  # compute the drowsiness score based on blink, yawning, duration and time
 
-    if drowsiness_score > DROWSINESS_SCORE_THRESHOLD:  # check if the drowsiness score is above the threshold
-        COUNTER += 1  # increment the frame counter
-        if COUNTER >= FRAMES_THRESHOLD:  # check if the drowsiness score is high for a sufficient number
-            if not ALARM_ON:  # check if the alarm is not on
-                ALARM_ON = True  # turn the alarm on
-                # start a thread to have the alarm sound played in the background
-                t = Thread(target=sound_alarm, args=(ALARM,))
-                t.deamon = True
-                t.start()
-            cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)  # draw an alarm on the frame
+        if drowsiness_score > DROWSINESS_SCORE_THRESHOLD:  # check if the drowsiness score is above the threshold
+            counter += 1  # increment the frame counter
+            if counter >= FRAMES_THRESHOLD:  # check if the drowsiness score is high for a sufficient number
+                if not alarm_on:  # check if the alarm is not on
+                    alarm_on = True  # turn the alarm on
+                    # start a thread to have the alarm sound played in the background
+                    t = Thread(target=sound_alarm, args=(ALARM,))
+                    t.deamon = True
+                    t.start()
+                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)  # draw an alarm on the frame
 
-    else:  # drowsiness score is below the threshold
-        COUNTER = 0  # reset the counter
-        ALARM_ON = False  # reset the alarm
-        cv2.putText(frame, "Drowsiness Score: {:.2f}".format(drowsiness_score), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)  # draw the drowsiness score on the frame
+        else:  # drowsiness score is below the threshold
+            counter = 0  # reset the counter
+            alarm_on = False  # reset the alarm
+            cv2.putText(frame, "Drowsiness Score: {:.2f}".format(drowsiness_score), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)  # draw the drowsiness score on the frame
 
-    # show the frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-    # if the `q` key was pressed, cleanup and break from the loop
-    if key == ord("q"):
-        cv2.destroyAllWindows()
-        vs.stop()
-        break
+        # if the `q` key was pressed, cleanup and break from the loop
+        if key == ord("q"):
+            cv2.destroyAllWindows()
+            vs.stop()
+            break
