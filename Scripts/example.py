dd8ea740c638c3aeef2ec8fede21fb9bac196ad7
ortal calfon <ortal.calfon@gmail.com>
2021-12-13 00:52:50 +0200

    example

---
+++ Scripts/example.py
@@ -0,0 +1,53 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils import face_utils
+import imutils
+import time
+import dlib
+import cv2
+import os
+
+WORKING_DIRECTORY = os.getcwd().replace("\\", "/").replace("Scripts", "")  # path to working directory
+SHAPE_PREDICTOR = WORKING_DIRECTORY + "Data/shape_predictor_68_face_landmarks.dat"  # path to facial landmark predictor
+WEBCAM = 0  # index of webcam on system
+# initialize dlib's face detector (HOG-based) and then load our
+# trained shape predictor
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
+# initialize the video stream and allow the cammera sensor to warmup
+print("[INFO] camera sensor warming up...")
+vs = VideoStream(src=WEBCAM).start()
+time.sleep(1.0)
+while True:
+    # grab the frame from the video stream, resize it to have a
+    # maximum width of 400 pixels, and convert it to grayscale
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+    # loop over the face detections
+    for rect in rects:
+    # convert the dlib rectangle into an OpenCV bounding box and
+    # draw a bounding box surrounding the face
+       (x, y, w, h) = face_utils.rect_to_bb(rect)
+       cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # use our custom dlib shape predictor to predict the location
+    # of our landmark coordinates, then convert the prediction to
+    # an easily parsable NumPy array
+       shape = predictor(gray, rect)
+       shape = face_utils.shape_to_np(shape)
+    # loop over the (x, y)-coordinates from our dlib shape
+    # predictor model draw them on the image
+       for (sX, sY) in shape:
+           cv2.circle(frame, (sX, sY), 1, (0, 0, 255), -1)
+    # show the frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+# do a bit of cleanup
+vs.stream.release()
+cv2.destroyAllWindows()
