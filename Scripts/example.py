7c5971321888bfe29804398dcf7850658c90eef8
Judit <yuhalper@g.jct.ac.il>
2021-12-16 22:01:59 +0200

    example

--- Scripts/example.py
+++ Scripts/example.py
@@ -9,22 +9,22 @@ import imutils
 import dlib
 import cv2
 
+# initialize the face detector
+detector = dlib.get_frontal_face_detector()
 
-detector = dlib.get_frontal_face_detector()  # initialize the face detector
-predictor = dlib.shape_predictor("../Data/shape_predictor_68_face_landmarks.dat")  # load the trained shape predictor
+# load the trained shape predictor
+predictor = dlib.shape_predictor("../Data/shape_predictor_68_face_landmarks.dat")
 
-vs = VideoStream(src=0).start()  # initialize the video stream, 0 is the webcam index on the system
+# initialize the video stream (0 is the webcam index on the system)
+vs = VideoStream(src=0).start()
 
-while True:  # loop over video frames
+while True:  # loop over the video frames
 
     frame = vs.read()  # grab the frame from the video stream
     frame = imutils.resize(frame, width=400)  # resize the frame to have a maximum width of 400 pixels
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert the frame to grayscale
 
     face = detector(gray_frame, 0)[0]  # detect a single face in the grayscale frame
-
-    (x, y, w, h) = face_utils.rect_to_bb(face)  # convert the Dlib rectangle into an OpenCV bounding box
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # draw a bounding box surrounding the face
 
     shape = predictor(gray_frame, face)  # predict the location of the face landmark coordinates
     shape = face_utils.shape_to_np(shape)  # convert the prediction to an easily parsable NumPy array
@@ -42,3 +42,9 @@ while True:  # loop over video frames
 # cleanup
 vs.stream.release()
 cv2.destroyAllWindows()
+
+
+"""
+(x, y, w, h) = face_utils.rect_to_bb(face)  # convert the Dlib rectangle into an OpenCV bounding box
+cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # draw a bounding box surrounding the face
+"""
