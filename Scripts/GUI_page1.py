31296c3b0f8e429a6b73e42e3120fa45245bd6f0
Naama-Shenberger <shanberg@g.jct.ac.il>
2022-01-08 22:37:06 +0200

    Page1_Gui

--- Scripts/GUI_page1.py
+++ Scripts/GUI_page1.py
@@ -14,7 +14,7 @@ regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 def check(email):
     # pass the regular expression
     # and the string into the fullmatch() method
-    if (re.fullmatch(regex, email)):
+    if re.fullmatch(regex, email):
         return 0
 
     else:
